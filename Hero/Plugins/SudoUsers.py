1766cb1c99954106cd54ad9245259bc8506742bc
emirpng <marveldarkbull@gmail.com>
2022-07-20 00:32:56 +0300

    Update SudoUsers.py

--- Hero/Plugins/SudoUsers.py
+++ Hero/Plugins/SudoUsers.py
@@ -92,7 +92,7 @@ async def userdel(_, message: Message):
         removed = await remove_sudo(user.id)
         if removed:
             await message.reply_text(
-                f"**{user.mention},** {MUSIC_BOT_NAME}'s Sudo listesinden kaldırıldı."
+                f"**{message.reply_to_message.from_user.mention},** {MUSIC_BOT_NAME}'s Sudo listesinden kaldırıldı."
             )
         await message.reply_text(f"Something wrong happened.")
         return
