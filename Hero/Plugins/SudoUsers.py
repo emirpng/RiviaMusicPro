a46c0306a3e0ed8834fda304d8f9d575420f9692
emirpng <marveldarkbull@gmail.com>
2022-07-19 23:54:46 +0300

    Update SudoUsers.py

--- Hero/Plugins/SudoUsers.py
+++ Hero/Plugins/SudoUsers.py
@@ -57,6 +57,7 @@ async def useradd(_, message: Message):
             await message.reply_text(
                 f"**{user.mention}** sudo kullanıcı olarak eklendi."
             )
+            os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
         else:
             await message.reply_text("Hata oluştu")
         return
@@ -69,6 +70,7 @@ async def useradd(_, message: Message):
         await message.reply_text(
             f"**{message.reply_to_message.from_user.mention}** sudo kullanıcı olarak eklendi."
         )
+        os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
     else:
         await message.reply_text("Failed")
     return
@@ -81,6 +83,7 @@ async def userdel(_, message: Message):
             await message.reply_text(
                 "Bir kullanıcının mesajını yanıtlayın veya username/user_id verin."
             )
+            return
         user = message.text.split(None, 1)[1]
         if "@" in user:
             user = user.replace("@", "")
@@ -91,9 +94,9 @@ async def userdel(_, message: Message):
         removed = await remove_sudo(user.id)
         if removed:
             await message.reply_text(
-                f"Removed **{user.mention}** from {MUSIC_BOT_NAME}'s Sudo."
+                f"**{user.mention},** {MUSIC_BOT_NAME}'s Sudo listesinden kaldırıldı."
             )
-             return
+            return os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
         await message.reply_text(f"Something wrong happened.")
         return
     from_user_id = message.from_user.id
@@ -106,8 +109,9 @@ async def userdel(_, message: Message):
     removed = await remove_sudo(user_id)
     if removed:
         await message.reply_text(
-            f"Removed **{mention}** from {MUSIC_BOT_NAME}'s Sudo."
+            f"**{user.mention},** {MUSIC_BOT_NAME}'s Sudo listesinden kaldırıldı."
         )
+        return os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
     await message.reply_text(f"Something wrong happened.")
 
 
