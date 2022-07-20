db0f3710e8784f2d537730a3b20d81b0b816c478
emirpng <marveldarkbull@gmail.com>
2022-07-20 17:45:04 +0300

    Update SudoUsers.py

--- Hero/Plugins/SudoUsers.py
+++ Hero/Plugins/SudoUsers.py
@@ -57,7 +57,7 @@ async def useradd(_, message: Message):
             await message.reply_text(
                 f"**{message.reply_to_message.from_user.mention}** sudo kullanıcı olarak eklendi."
             )
-            os.system(f"python3 -m Hero")
+            os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
         else:
             await message.reply_text("Hata oluştu")
         return
@@ -70,7 +70,7 @@ async def useradd(_, message: Message):
         await message.reply_text(
             f"**{user.mention}** sudo kullanıcı olarak eklendi."
         )
-        os.system(f"python3 -m Hero")
+        os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
     else:
         await message.reply_text("Failed")
     return
@@ -96,7 +96,7 @@ async def userdel(_, message: Message):
             await message.reply_text(
                 f"**{message.reply_to_message.from_user.mention}** {MUSIC_BOT_NAME}'s Sudo listesinden kaldırıldı."
             )
-            return os.system(f"python3 -m Hero")
+            return os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
         await message.reply_text(f"Something wrong happened.")
         return
     from_user_id = message.from_user.id
@@ -111,7 +111,7 @@ async def userdel(_, message: Message):
         await message.reply_text(
             f"**{message.reply_to_message.from_user.mention},** {MUSIC_BOT_NAME}'s Sudo listesinden kaldırıldı."
         )
-        return os.system(f"python3 -m Hero")
+        return os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
     await message.reply_text(f"Something wrong happened.")
 
 
