7798f175b785fa16f90260fb8192019d1fc845ba
emirpng <marveldarkbull@gmail.com>
2022-07-20 23:41:29 +0300

    Update download.py

--- Hero/Utilities/download.py
+++ Hero/Utilities/download.py
@@ -48,39 +48,6 @@ async def get_formats(CallbackQuery, videoid, user_id, type):
     j = 0
     for x in formats:
         check = x["format"]
-        if type == "audio":
-            if "audio" in check:
-                j += 1
-                if j == 1:
-                    a1 = InlineKeyboardButton(
-                        text=f"Ses 🎵 {humanbytes(x['filesize'])}",
-                        callback_data=f"ytdata audio||{x['format_id']}||{videoid}",
-                    )
-                if j == 2:
-                    a2 = InlineKeyboardButton(
-                        text=f"Ses 🎵 {humanbytes(x['filesize'])}",
-                        callback_data=f"ytdata audio||{x['format_id']}||{videoid}",
-                    )
-                if j == 3:
-                    a3 = InlineKeyboardButton(
-                        text=f"Ses 🎵 {humanbytes(x['filesize'])}",
-                        callback_data=f"ytdata audio||{x['format_id']}||{videoid}",
-                    )
-                if j == 4:
-                    a4 = InlineKeyboardButton(
-                        text=f"Ses 🎵 {humanbytes(x['filesize'])}",
-                        callback_data=f"ytdata audio||{x['format_id']}||{videoid}",
-                    )
-                if j == 5:
-                    a5 = InlineKeyboardButton(
-                        text=f"Ses 🎵 {humanbytes(x['filesize'])}",
-                        callback_data=f"ytdata audio||{x['format_id']}||{videoid}",
-                    )
-                if j == 6:
-                    a6 = InlineKeyboardButton(
-                        text=f"Ses 🎵 {humanbytes(x['filesize'])}",
-                        callback_data=f"ytdata audio||{x['format_id']}||{videoid}",
-                    )
         elif type == "video":
             if str(133) in check:
                 j += 1
