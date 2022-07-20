3e94dbd0135668be6c0e5a8bb06e0f5049762a78
emirpng <marveldarkbull@gmail.com>
2022-07-20 04:01:18 +0300

    Update SudoUsers.py

--- Hero/Plugins/SudoUsers.py
+++ Hero/Plugins/SudoUsers.py
@@ -57,7 +57,7 @@ async def useradd(_, message: Message):
             await message.reply_text(
                 f"**{user.mention}** sudo kullanıcı olarak eklendi."
             )
-            os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
+            os.system(f"kill -9 {os.getpid()}")
         else:
             await message.reply_text("Hata oluştu")
         return
@@ -70,7 +70,7 @@ async def useradd(_, message: Message):
         await message.reply_text(
             f"**{message.reply_to_message.from_user.mention}** sudo kullanıcı olarak eklendi."
         )
-        os.system(f"kill -9 {os.getpid()} && python3 -m Yukki")
+        os.system(f"kill -9 {os.getpid()})
     else:
         await message.reply_text("Failed")
     return
