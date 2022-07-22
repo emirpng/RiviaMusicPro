78beaf9b97a8d26e174bd87628554064749c53bc
emirpng <marveldarkbull@gmail.com>
2022-07-22 04:12:22 +0300

    Update func.py

--- Hero/Utilities/func.py
+++ Hero/Utilities/func.py
@@ -83,7 +83,7 @@ async def mplay_stream(message,MusicData):
         return await message.reply_text(
             f"**Süre limiti aşıldı**\n\n**İzin verilen: **{DURATION_LIMIT_MIN} dakika\n**Oynatmak istediğiniz::** {duration_min} dakika"
         )
-    mystic = await message.reply_text(f"🔄 İşleniyor..:- {title[:20]}")
+    mystic = await message.reply_text(f"🔄 Müzik İndiriliyor... Lütfen Bekleyin... {title[:20]}")
     await mystic.edit(
         f"**{MUSIC_BOT_NAME} İndirici**\n**Şarkı:** {title[:50]}\n\n0% ▓▓▓▓▓▓▓▓▓▓▓▓ 100%"
     )
